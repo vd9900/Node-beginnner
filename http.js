255233395cbe01c8032b112fe091e68227ac1d00
Vinith <vinithdevadiga7@gmail.com>
2022-08-29 22:33:55 +0530

    http module added

---
+++ http.js
@@ -0,0 +1,20 @@
+const http = require("http")
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.write("welcome to http module home page")
+        res.end();
+    }
+    if (req.url === "/contact") {
+        res.write("welcome to contact page 24/7")
+        res.end();
+    }
+    if (req.url === "/login") {
+        res.write("Enter your name and password in query parmerter")
+        res.end();
+    }
+});
+server.on("connection", (socket) => {
+    console.log("New connection request");
+})
+server.listen(5555)
+
