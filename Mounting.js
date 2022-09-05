8667c9e964c032885df95562fb2bb487a3520bb6
Vinith <vinithdevadiga7@gmail.com>
2022-09-05 13:07:49 +0530

    mounting.js file updated

---
+++ Mounting.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+
+app.use(express.json())
+
+
+// Mountiing is basically to make clean code and readable in code like in all crud methods will messy around so we 
+// use montuning idology
+
