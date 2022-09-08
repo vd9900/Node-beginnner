eccb0baff842b819b2219e7273948f6d389936ae
Vinith <vinithdevadiga7@gmail.com>
2022-09-08 13:52:22 +0530

    session.js added

--- Cookies.js
+++ Cookies.js
@@ -1,22 +1,30 @@
 const express = require("express");
-const cookiesParser = require("cookies-parser")
+const cookiesParser = require("cookie-parser")
 const app = express()
+cookiesParser()
 
 const Router = express.Router();
+
+app.use(cookiesParser())
+
 
 //user Object
 const user = {
     user :"vinith"
 }
-app.use(cookiesparser)
 app.use("/",Router)
 
 Router.route("/home").get(homeGET)
 Router.route("/setuser").get(setUserGET)
+Router.route("/getuser").get(getUserGET)
 
 function setUserGET(req,res) {
-    res.cookies("user",user)
+    res.cookie("user",user)
     res.send("Your account created you can go now to dashboard")
+}
+function getUserGET(req,res) {
+ 
+    res.send(req.cookies.user)
 }
 function homeGET(req,res){
     res.send("welcome to home page")
